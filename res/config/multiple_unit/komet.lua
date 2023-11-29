a4166288f54ef7da41905f39a1c967c0f1d6265a
Florian Thiel <flth@flth.de>
2023-11-29 23:35:23 +0100

    Komet hinzugefügt

---
+++ res/config/multiple_unit/komet.lua
@@ -0,0 +1,24 @@
+﻿function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/vt10.5/komet/kopf1.mdl", forward = true },
+		{ name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen1.mdl", forward = true },
+		{ name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen1.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen2.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen3.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen1.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/waggon/vt10.5/komet/wagen5.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/jakobs.mdl", forward = true },
+        { name = "vehicle/train/vt10.5/komet/kopf2.mdl", forward = false },
+	},
+	name = _("komet_name"),
+	desc = _("komet_desc"),
+    groupFileName = "vehicle/train/vt10.5/menu_vt10.5.mdl",
+}
+end
