c7b743363eae2db811d4cabca611d0b125381f68
Florian Thiel <flth@flth.de>
2023-11-29 23:35:58 +0100

    Strings angepasst

--- strings.lua
+++ strings.lua
@@ -2,16 +2,16 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "DB VT 10.5 Senator",
-		["mod_desc"] = "Two articulated trains from the German Federal Railway were referred to as VT 10.5, one of which was purchased together. VT 10 501 Senator was intended for daytime use, VT 10 551 Komet for nighttime use. A third train for the Deutsche Bundespost was no longer built.\nBoth multiple units were equipped with two motors in the power cars for the drive and two additional small motors for the power supply. Both trains were made of aluminum alloys, were quite maintenance-sensitive and were only in use for a very short time.",
+		["mod_name"] = "DB VT 10.5 Senator/Komet",
+		["mod_desc"] = "Two articulated trains from the Deutsche Bundesbahn were referred to as VT 10.5, one of which was purchased together. VT 10 501 Senator was intended for daytime use, VT 10 551 Komet for nighttime use. A third train for the Deutsche Bundespost was no longer built.\nBoth multiple units were equipped with two motors in the power cars for the drive and two additional small motors for the power supply. Both trains were made of aluminum alloys, were quite maintenance-sensitive and were only in use for a very short time.",
 
-        ["vt10.5_menu"] = "VT 10.5 Senator",
+        ["vt10.5_menu"] = "VT 10.5 Senator/Komet",
 
         ["senator_1_name"] = "VT 10.5 Senator (1954)",
         ["senator_2_name"] = "VT 10.5 Senator (1954)",
-        ["senator_2_desc"] = "Two articulated trains from the German Federal Railway were referred to as VT 10.5, one of which was purchased together. The VT 10 501 Senator was intended for daytime use. After a few days in use, an accident occurred in 1954 after the front of the train was slightly modified and repainted. It remained in use until 1959, but did not prove successful due, among other things, to the very hard chassis.",
+        ["senator_2_desc"] = "Two articulated trains from the Deutsche Bundesbahn were referred to as VT 10.5, one of which was purchased together. The VT 10 501 Senator was intended for daytime use. After a few days in use, an accident occurred in 1954 after the front of the train was slightly modified and repainted. It remained in use until 1959, but did not prove successful due, among other things, to the very hard chassis.",
         ["komet_name"] = "VT 10.5 Komet (1954)",
-        ["komet_desc"] = "Two articulated trains from the German Federal Railway were referred to as VT 10.5, one of which was purchased together. The VT 10 551 Komet was intended for use at night and consisted of cars with sleeping compartments. The multiple unit proved successful, but was quite expensive due to a total of 6 engines (4 for driving, 2 for the auxiliary operations) and as a single example it could only run between Hamburg and Basel every 2 days. He was decommissioned in October 1957.",
+        ["komet_desc"] = "Two articulated trains from the Deutsche Bundesbahn were referred to as VT 10.5, one of which was purchased together. The VT 10 551 Komet was intended for use at night and consisted of cars with sleeping compartments. The multiple unit proved successful, but was quite expensive due to a total of 6 engines (4 for driving, 2 for the auxiliary operations) and as a single example it could only run between Hamburg and Basel every 2 days. He was decommissioned in October 1957.",
 
         ["senator_kopf"] = "Power head VT 10 501 Senator",
         ["senator_wagen_c_f"] = "Centre carriage c and f, Seats",
@@ -20,25 +20,24 @@ return {
         ["senator_wagen_g"] = "Centre carriage g, Cloakroom, boot, two toilets",
 
         ["komet_kopf"] = "Power head VT 10 551 Komet",
-        ["komet_wagen_c"] = "Centre carriage c, with toilet, washroom, 2 DSG service compartments, boot",
-        ["komet_wagen_d"] = "Centre carriage d, with toilet, 2 luggage compartments",
+        ["komet_wagen_c_d"] = "Centre carriage c, with toilet, luggage storage",
         ["komet_wagen_e"] = "Centre carriage e, Kitchen with buffet and bar",
         ["komet_wagen_f"] = "Centre carriage f, Toilet, luggage, laundry and storage rooms",
         ["komet_wagen_g"] = "Centre carriage g, Service compartment, washroom, luggage, laundry and storage rooms",
-        ["komet_wagen_h"] = "Dining room with 21 seats",
+        ["komet_wagen_h"] = "Centre carriage h, Dining room with 21 seats",
 
 	},
 	de = {
 
-		["mod_name"] = "DB VT 10.5 Senator",
+		["mod_name"] = "DB VT 10.5 Senator/Komet",
 		["mod_desc"] = "Als VT 10.5 wurden zwei Gliederzüge der Deutschen Bundesbahn bezeichnet, die gemeinsam beschafft wurden in je einem Exemplar. VT 10 501 Senator war für den Tageinsatz vorgesehen, VT 10 551 Komet für die Nacht. Ein dritter Zug für die Deutsche Bundespost wurde nicht mehr realisiert.\nBeide Triebzügen waren in den Triebköpfen mit jeweils 2 Motoren für den Antrieb ausgerüstet sowie je zwei weiteren kleinen Motoren für die Stromversorgung. Beide Zügen waren aus Aluminiumlegierungen hergestellt, waren recht wartungsanfällig und jeweils nur recht kurze Zeit im Einsatz.",
 
-        ["vt10.5_menu"] = "VT 10.5 Senator",
+        ["vt10.5_menu"] = "VT 10.5 Senator/Komet",
 
-        ["senator_1_name"] = "VT 10.5 Senator (1953)",
-        ["senator_2_name"] = "VT 10.5 Senator (1953)",
+        ["senator_1_name"] = "VT 10.501 Senator (1953)",
+        ["senator_2_name"] = "VT 10.501 Senator (1953)",
         ["senator_2_desc"] = "Als VT 10.5 wurden zwei Gliederzüge der Deutschen Bundesbahn bezeichnet, die gemeinsam beschafft wurden in je einem Exemplar. Der VT 10 501 Senator war für den Einsatz am Tag vorgesehen. Nach wenigen Tagen im Einsatz kam es 1954 zu einem Unfall, nachdem der Zug an der Front etwas umgebaut wurde und neu lackiert. Es blieb noch bis 1959 im Einsatz, bewährte sich aber nicht u.a. aufgrund des sehr harten Fahrwerks.",
-        ["komet_name"] = "VT 10.5 Komet (1953)",
+        ["komet_name"] = "VT 10.551 Komet (1953)",
         ["komet_desc"] = "Als VT 10.5 wurden zwei Gliederzüge der Deutschen Bundesbahn bezeichnet, die gemeinsam beschafft wurden in je einem Exemplar. Der VT 10 551 Komet war für den Einsatz in der Nacht vorgesehen und bestand aus Wagen mit Schlafabteilen. Der Triebzug bewährte sich, war allerdings recht kostenintensiv durch insgesamt 6 Motoren (4 zum Antrien, 2 für die Hilfsbetriebe) und als Einzelexemplar konnte er nur alle 2 Tage zwischen Hamburg und Basel verkehren. Er wurde im Oktober 1957 abgestellt.",
 
         ["senator_kopf"] = "Triebkopf VT 10 501 Senator",
@@ -48,12 +47,11 @@ return {
         ["senator_wagen_g"] = "Mittelwagen g, Garderobe, Kofferraum, zwei Toiletten",
 
         ["komet_kopf"] = "Triebkopf VT 10 551 Komet",
-        ["komet_wagen_c"] = "Mittelwagen c, mit Toilette, Waschraum, 2 DSG-Dienstabteile, Kofferraum",
-        ["komet_wagen_d"] = "Mittelwagen d, mit Toilette, 2 Kofferräume",
+        ["komet_wagen_c_d"] = "Mittelwagen c, mit Toilette, Kofferraum",
         ["komet_wagen_e"] = "Mittelwagen e, Küche mit Buffet und Bar",
         ["komet_wagen_f"] = "Mittelwagen f, Toilette, Koffer-, Wäsche- und Vorratsräume",
         ["komet_wagen_g"] = "Mittelwagen g, Dienstabteil, Waschraum, Koffer-, Wäsche- und Vorratsräume",
-        ["komet_wagen_h"] = "Speiseraum mit 21 Plätzen",
+        ["komet_wagen_h"] = "Mittelwagen h, Speiseraum mit 21 Plätzen",
 
 	},
 }
